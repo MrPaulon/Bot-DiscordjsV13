757f3b7eb3c5af33023f42f1e65c53185a915c67
Paul <76972320+MrPaulon@users.noreply.github.com>
2021-12-21 15:20:00 +0100

    Modifications général du code

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
 const { Client, Intents } = require('discord.js');
 const { clientId, guildId, token } = require('./auth.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES] });
-const guild = client.guilds.cache.get(guildId)
 
-let commands
-if (guild) {
-  commands = guild.commands
-} else {
-  commands = client.application?.commands
-}
+client.on('ready', () => {
+  console.log('Le bot est en ligne')
 
-commands?.create({
-    name: 'add',
-    description: 'Adds two numbers',
-    options: [
-      {
-        name: 'number1',
-        description: 'The first number.',
-        required: true,
-        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-      },
-      {
-        name: 'number2',
-        description: 'The second number.',
-        required: true,
-        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-      },
-    ],
+  const guild = client.guilds.cache.get(guildId)
+  let commands
+
+  if (guildId) {
+    commands = guild.commands
+  } else {
+    commands = client.application?.commands
+  }
+
+  commands?.create({
+    name: 'ping',
+    description: "Réponds pong",
+  })
 })
 
-// Listen for when the bot is ready
-client.on('ready', () => {
-    console.log('The bot is ready!')
-  })
-  // Listen for new messages
-  client.on('messageCreate', (msg) => {
-    // Was the message "ping"?
-    if (msg.content === 'ping') {
-      // Reply with "pong"
-      msg.reply({
-        content: 'pong',
-      })
-    }
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()){
+    return
+  }
+
+  const {commandName, options} = interaction
+
+  if (commandName == 'ping') {
+    interaction.reply({
+      content: 'pong',
+      ephemeral: true, 
+    })
+  }
 })
 
 client.login(token)
